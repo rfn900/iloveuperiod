4ae38ef3569cb02d0f892a8acca2e1a263ac0bbb
rfn900 <rfn900@gmail.com>
2021-11-11 14:00:19 +0100

    Add Next Head and Favicon

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,15 @@
+import Head from "next/head";
 import Nav from "./Nav";
 import Footer from "./Footer";
 
 export default function Layout({ children }) {
   return (
     <div className="flex min-h-screen flex-col justify-between ">
+      <Head>
+        <title>I Love U Period</title>
+        <link rel="icon" href="/iloveuperiod-favicon.png" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Nav />
       <main>{children}</main>
       <Footer />
