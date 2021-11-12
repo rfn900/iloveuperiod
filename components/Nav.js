fa5e6c74f7ab2ab6993c2524830d4c938f7f901b
rfn900 <rfn900@gmail.com>
2021-11-12 10:45:53 +0100

    Change Nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,17 +1,43 @@
 import Link from "next/link";
+import { BsHandbagFill } from "react-icons/bs";
+import { BsSearch } from "react-icons/bs";
 
 function Nav() {
   return (
-    <header className="border-b sticky top-0 z-20 bg-white">
+    <header className="absolute w-screen top-0 z-20 text-gray-50 lg:py-4 bg-transparent">
       <div className="flex items-center justify-between max-w-6xl pt-4 pb-2 px-4 mx-auto lg:max-w-screen-xl">
         <Link href="/" passHref>
           <a className="cursor-pointer">
-            <span className="text-lg pt-1 font-bold">I LOVE U PERIOD</span>
+            <p className="text-lg font-thin text-2xl lg:text-4xl">
+              I LOVE U <span className="font-normal">PERIOD</span>
+            </p>
           </a>
         </Link>
-        <div className="flex items-center gap-8">
-          <BsSearch className="w=6 h-7" />
-          <BsHandbagFill className="w-7 h-7" />
+
+        <div className="hidden lg:flex gap-8 font-thin text-lg tracking-wider">
+          <Link href="/" passHref>
+            <a className="cursor-pointer">
+              <span className="text-lg">Shop</span>
+            </a>
+          </Link>
+          <Link href="/" passHref>
+            <a className="cursor-pointer">
+              <span className="text-lg">Collections</span>
+            </a>
+          </Link>
+          <Link href="/" passHref>
+            <a className="cursor-pointer">
+              <span className="text-lg">More About Our Panties</span>
+            </a>
+          </Link>
+        </div>
+        <div className="flex items-center gap-4 lg:gap-8">
+          <BsSearch className="w-4 lg:w-6 h-4 lg:h-6 mt-1 hidden lg:block" />
+          <BsHandbagFill className="w-5 h-5 lg:w-7 lg:h-7" />
+          <div className="flex flex-col gap-[8px] lg:hidden">
+            <div className="w-6 h-px bg-gray-50"></div>
+            <div className="w-6 h-px bg-gray-50"></div>
+          </div>
         </div>
       </div>
     </header>
