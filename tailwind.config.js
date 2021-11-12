cc35d7eef6e629503df68be46b3198dee4179791
rfn900 <rfn900@gmail.com>
2021-11-12 10:45:32 +0100

    Add brand colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "brand-red": "#900503",
+        "brand-red-light": "#FDB9B9",
+        "brand-khaki": "#C49779",
+        "brand-khaki-light": "#F3D8C3",
+        "brand-lavanda": "#DFC4C3",
+        "brand-lavanda-light": "#F0E6E6",
+        "brand-green": "#1E726B",
+        "brand-green-light": "#B2D8D5",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
